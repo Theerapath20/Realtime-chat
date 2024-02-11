c12d078be6cfdde22423a91aec812769a03257b3
Theerapath20 <123882263+Theerapath20@users.noreply.github.com>
2024-02-11 09:59:59 +0700

    create message and conversation DB  && update user DB

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,1 +1,26 @@
 import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      requires: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      requires: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    // createAT , updateAt
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message
