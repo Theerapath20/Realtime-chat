27871ad4e4d1b1cc9ab58d935bfde413c0efc543
Theerapath20 <123882263+Theerapath20@users.noreply.github.com>
2024-02-11 10:04:24 +0700

    import cookie parser && import message route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,9 +14,14 @@ const PORT = process.env.PORT || 5000;
 //   // root http://localhost:5000/
 //   res.send("Hello world 55555");
 // });
+
 app.use(express.json()); // to parse the incoming requests withJSON payload (from raq.body)
+app.use(cookieParser());
+
+
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
